6ddacea40a60f48355d30a6943efc893fdb8f8b5
ryanthomas92 <rthoma11@villanova.edu>
2017-01-15 15:43:35 -0800

    anchor tagged the titles of songs to get lyric on lyric page, hard coded lyrics to one song

--- public/scripts/controllers/LyricsShowController.js
+++ public/scripts/controllers/LyricsShowController.js
@@ -6,6 +6,68 @@ LyricsShowController.$inject = ['$http'];
 
 function LyricsShowController($http) {
   var vm = this;
+
+  vm.lyrics = [
+    {
+      lines: "Early in the morning, risin' to the street"
+    },
+    {
+      lines: "Light me up that cigarette and I strap shoes on my feet"
+    },
+    {
+      lines: "Got to find a reason, a reason things went wrong"
+    },
+    {
+      lines: "Got to find a reason why my money's all gone"
+    },
+    {
+      lines: "I got a dalmatian, and I can still get high"
+    },
+    {
+      lines: "I can play the guitar like a motherfucking riot"
+    },
+    {
+      lines: "Well, life is too short, so love the one you got"
+    },
+    {
+      lines: "'Cause you might get run over or you might get shot"
+    },
+    {
+      lines: "Never start no static I just get it off my chest"
+    },
+    {
+      lines: "Never had to battle with no bulletproof vest"
+    },
+    {
+      lines: "Take a small example, take a tip from me"
+    },
+    {
+      lines: "Take all of your money, give it all to charity"
+    },
+    {
+      lines: "Love is what I got"
+    },
+    {
+      lines: "It's within my reach"
+    },
+    {
+      lines: "And the Sublime style's still straight from Long Beach"
+    },
+    {
+      lines: "It all comes back to you, you'll finally get what you deserve"
+    },
+    {
+      lines: "Try and test that you're bound to get served"
+    },
+    {
+      lines: "Love's what I got"
+    },
+    {
+      lines: "Don't start a riot"
+    }
+  ]
+
+
   vm.newLyrics = {};
 
 
